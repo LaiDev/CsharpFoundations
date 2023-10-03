a8a96bbe5fa416f0b0dd317bb490c469b8c09d97
Lai <l.bnjamin0309@gmail.com>
2023-10-03 13:54:49 -0400

    ADD: Solution to Business Rules Challenge

---
+++ logic/Program.cs
@@ -0,0 +1,34 @@
+﻿// Challenge: Improve renewal rate of subscriptions
+/* You've been asked to add a feature to your company's software. The feature is intended to improve the renewal rate of subscriptions to the software. Your task is to display a renewal message when a user logs into the software system and is notified their subscription will soon end. You'll need to add a couple of decision statements to properly add branching logic to the application to satisfy the requirements. */
+
+Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+// Your code goes here
+
+Console.WriteLine(daysUntilExpiration);
+
+    if(daysUntilExpiration == 0)
+{
+    Console.WriteLine("Your subscription has expired.");
+} else if(daysUntilExpiration <= 1)
+{
+    Console.WriteLine("Your subscription expires within a day! Renew now and save 20%!");
+} else if(daysUntilExpiration <= 5)
+{
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days. Renew now and save 10%!");
+} else if(daysUntilExpiration <= 10)
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
+
+if(daysUntilExpiration <= 1)
+{
+    discountPercentage = 20;
+    Console.WriteLine($"Your Discount Percentage: {discountPercentage}%");
+} else if(daysUntilExpiration <= 5)
+{
+    discountPercentage = 10;
+    Console.WriteLine($"Your Discount Percentage: {discountPercentage}%");
+}
