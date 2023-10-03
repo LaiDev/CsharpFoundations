4f7e6fc2e393fd4cc027f99cf74de4c0086141a8
Lai <l.bnjamin0309@gmail.com>
2023-10-03 17:37:23 -0400

    ADD: Solution to Order Id Challenge

--- array/Program.cs
+++ array/Program.cs
@@ -40,3 +40,15 @@ foreach (int number in numbers)
 }
 
 Console.WriteLine($"Adding all the numbers together gives you {total}!");
+
+//Challenge: Report the Order IDs that need further investigation
+//Your team has found a pattern. Orders that start with the letter "B" encounter fraud at a rate 25 times greater than the normal rate. You write new code that outputs the Order ID of new orders where the Order ID starts with the letter "B". This will be used by the fraud team to investigate further.
+
+String[] orders = {"B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"};
+foreach (string order in orders)
+{
+    if(order.StartsWith("B"))
+    {
+    Console.WriteLine(order);
+    }
+}
