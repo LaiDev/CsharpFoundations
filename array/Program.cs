7863676e8417131a4e57c0f49d0f0e0af9487133
Lai <l.bnjamin0309@gmail.com>
2023-10-03 17:18:46 -0400

    ADD: Assigning Values in an Array

---
+++ array/Program.cs
@@ -0,0 +1,9 @@
+﻿//The first set of square Brackets tells the compiler that the variable is an array
+
+//The second set of square brackets indicates the number of elements that the array can hold
+String[] colors = new string[3];
+
+//Assigning values to array elements
+colors[0] = "Blue";
+colors[1] = "Red";
+colors[2] = "Yellow";
